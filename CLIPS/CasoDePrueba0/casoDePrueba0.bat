a1d5cf032f630fbb31b5b2baed4a547a6824de9a
carlos.roldan <carlosroldanmontaner@gmail.com>
2016-12-05 16:44:11 +0100

    asdfsa

---
+++ CLIPS/CasoDePrueba0/casoDePrueba0.bat
@@ -0,0 +1,30 @@
+(load "F:/IA/Practica2-IA/CLIPS/Ontologia.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/MAINMOD.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/PreguntasMod.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/HeuristicMod.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/ObsTimeMod.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/PaintIntMod.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/VisitaMod.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/SortMod.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/CrearVisitaMod.CLP")
+(load "F:/IA/Practica2-IA/CLIPS/OrganizeMod.CLP")
+(reset)
+(run)
+Tonto
+1
+1
+10800
+2
+1
+1
+1
+2
+1
+1
+1
+1
+1
+done
+done
+done
+done
